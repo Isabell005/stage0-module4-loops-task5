0bd97c734c2b5f3ebdaf527403f7723c30d4dc15
Isabella Karibian <isabellakaribian@gmail.com>
2023-07-26 15:26:07 +0400

    stage0module4loopsTask5

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,25 +2,32 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-        int sp = 0; // there are 0 spaces in first row  int st = n; // there are n stars in first row
-        for (int i = 1; i <= height; i++) {
-            for (int j = 1; j <= sp; j++) // for printing spaces
+        int i, j, k;
+        for (i= 0; i<= height-1 ; i++)
+        {
+            for (j=0; j<i; j++)
             {
-                System.out.print("	");
+                System.out.print(" ");
             }
-            for (int j = 1; j <= height; j++) {
-                System.out.print("*	");
+            for (k=i; k<=height-1; k++)
+            {
+                System.out.print("8" + " ");
             }
-            if (i <= height / 2) {
-                height -= 2;
-                sp++;
-            } else {
-                height += 2;
-                sp--;
-            }
-
-            System.out.println();
+            System.out.println(" ");
         }
+        for (i= height-1; i>= 0; i--)
+        {
+            for (j=0; j<i; j++)
+            {
+                System.out.print(" ");
+            }
+            for (k=i; k<=height-1; k++)
+            {
+                System.out.print("8" + " ");
+            }
+            System.out.println(" ");
+        }
+
     }
 }
 
