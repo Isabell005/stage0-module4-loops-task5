cdf0812e186d4c3129379a8f616eba809ff752cf
Isabella Karibian <isabellakaribian@gmail.com>
2023-09-14 10:41:00 +0400

    stage0module4loopsTask5

--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -4,11 +4,10 @@ public class Triangle {
     public void printTriangle(int cathetusLength) {
 
         for (int i = 1; i <= cathetusLength; i++) {
-            // Print '8's in each row
             for (int j = 1; j <= i; j++) {
                 System.out.print("8");
             }
-            System.out.println(); // Move to the next line
+            System.out.println();
         }
     }
 }
