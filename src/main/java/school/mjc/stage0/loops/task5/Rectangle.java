1cccb0b2b3a71051af8a245e21352b8dcf266685
Isabella Karibian <isabellakaribian@gmail.com>
2023-09-14 10:40:05 +0400

    stage0module4loopsTask5

--- src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
@@ -2,18 +2,27 @@ package school.mjc.stage0.loops.task5;
 
 public class Rectangle {
     public void printRectangleFrom8s(int length, int height) {
-        for (int i = 1; i <= length; i++) {
-            for (int j = 1 ; j <= height; j++)
-                if((i==1 || i==height) || (j==1 || j==height))
-                    System.out.print("8");
-                else
-                    System.out.print(" ");
 
-            System.out.println();
+        for (int i = 0; i < length; i++) {
+            System.out.print("8");
         }
+        System.out.println(); // Move to the next line
+
+        // Print the sides and interior of the rectangle
+        for (int i = 2; i < height; i++) {
+            System.out.print("8");
+
+            for (int j = 2; j < length; j++) {
+                System.out.print(" ");
+            }
+            System.out.println("8");
+        }
+        for (int i = 0; i < length; i++) {
+            System.out.print("8");
+        }
+        System.out.println();
     }
 }
-
 
 
 
--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -2,11 +2,13 @@ package school.mjc.stage0.loops.task5;
 
 public class Triangle {
     public void printTriangle(int cathetusLength) {
-        for (int i = 0; i < cathetusLength; i++) {
-            for (int j = 0; j <= i; j++) {
+
+        for (int i = 1; i <= cathetusLength; i++) {
+            // Print '8's in each row
+            for (int j = 1; j <= i; j++) {
                 System.out.print("8");
             }
-            System.out.println();
+            System.out.println(); // Move to the next line
         }
     }
 }
